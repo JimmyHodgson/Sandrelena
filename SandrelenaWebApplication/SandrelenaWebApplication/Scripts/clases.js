4e16497c12ee52a5e73af769e02f5b0084f33ac5
Jimmy Hodgson <Jimmy Hodgson>
2016-05-01 12:06:48 -0600

    Se comienza con la implementación del front end para la información de clases.

---
+++ SandrelenaWebApplication/SandrelenaWebApplication/Scripts/clases.js
@@ -0,0 +1,5 @@
+﻿$(document).ready(function () {
+    $("#class-select").select2();
+    $('b[role="presentation"]').hide();
+    $('.select2-selection__arrow').append('<i class="fa fa-angle-down"></i>');
+});
