bc274cc7c49a80610ef8a4b3ce2132658e38e6be
Carlos <camposcarlos06@gmail.com>
2016-05-05 12:49:02 -0600

    Actualización del Modelo. Desplurarización de las tablas. Creación de la clase base Carrera.cs. Creación de un ejemplo de acceso a datos "PruebaEntity.aspx" en vistas.

---
+++ SandrelenaWebApplication/Business/Carrera.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+
+namespace Business
+{
+
+    public class Carrera
+    {
+        //EJEMPLO DE USO DE CLASES
+
+        #region Constructores
+
+        public Carrera()
+        {
+            //Constructor para la lista de carreras
+
+            _modelo = new SandrelenaCS();
+
+            var Carreras = (from c in _modelo.Carreras
+                            select new
+                            {
+                                IDCarrera = c.id_carrera,
+                                IDFacutlad = c.id_facultad,
+                                Nombre = c.nombre_carrera
+                            }).ToList();
+            _listado = Carreras;
+        }
+
+        public Carrera(int id)
+        {
+            //Constructor para instanciar una carrera
+
+            _modelo = new SandrelenaCS();
+
+            var carrera = (from c in _modelo.Carreras
+                            where c.id_carrera==id
+                            select new
+                            {
+                                Nombre = c.nombre_carrera,
+                                IDCarrera = c.id_carrera,
+                                IDFacultad = c.id_facultad
+                            }).Single();
+
+            _nombre = carrera.Nombre.ToString();
+            _idCarrera = carrera.IDCarrera;
+            _idFacultad = carrera.IDFacultad;
+
+        }
+
+        #endregion
+
+        #region Propiedades
+
+        private SandrelenaCS _modelo;
+
+        public dynamic _listado;
+        private string _nombre;
+        private int _idCarrera;
+        private int? _idFacultad;
+
+        public dynamic listado { get { return _listado; } }
+        public string nombre { get { return _nombre; } set { _nombre = value; } }
+        public int idCarrera { get { return _idCarrera; } set { _idCarrera = value; } }
+        public int? idFacultad { get { return _idFacultad; } set { _idFacultad = value; } }
+
+        #endregion
+
+        #region Métodos
+
+        //Parecido a las consultas de los constructores pero con updates, inserts, etc.; instanciando las clases del modelo.
+
+        #endregion
+
+    }
+}
---
+++ SandrelenaWebApplication/SandrelenaWebApplication/Views/PruebaEntity.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="PruebaEntity.aspx.cs" Inherits="SandrelenaWebApplication.Views.PruebaEntity" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:GridView ID="GridView1" runat="server">
+            <Columns>
+                <asp:BoundField HeaderText="IDCarrera" DataField="IDCarrera" />
+                <asp:BoundField HeaderText="Nombre" DataField="Nombre" />
+            </Columns>
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
