280f8a1a96811fe835bcbdcc75ea6233a2f7a239
Carlos <camposcarlos06@gmail.com>
2016-05-04 23:43:00 -0600

    Inclusión de un datagridview en matrícula para que se le apliquen estilos de prueba en el front end.

--- SandrelenaWebApplication/SandrelenaWebApplication/Views/matricula.aspx.cs
+++ SandrelenaWebApplication/SandrelenaWebApplication/Views/matricula.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +12,32 @@ namespace SandrelenaWebApplication.Views
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Page.IsPostBack==false)
+            {
 
+                DataTable dt = new DataTable();
+                DataRow dr = null;
+                dt.Columns.Add("ProductName", System.Type.GetType("System.String"));
+                dt.Columns.Add("CategoryName", System.Type.GetType("System.String"));
+                dt.Columns.Add("SupplierName", System.Type.GetType("System.String"));
+                dt.Columns.Add("UnitPrice", System.Type.GetType("System.Double"));
+                dt.Columns.Add("Discontinued", System.Type.GetType("System.String"));
+
+                for (int i = 0; i < 15; i++)
+                {
+                    dr = dt.NewRow();
+                    dr["ProductName"] = i.ToString();
+                    dr["CategoryName"] = i.ToString();
+                    dr["SupplierName"] = i.ToString();
+                    dr["UnitPrice"] = i;
+                    dr["Discontinued"] = i.ToString();
+                    dt.Rows.Add(dr);
+                }
+
+                dt.AcceptChanges();
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
         }
     }
 }
