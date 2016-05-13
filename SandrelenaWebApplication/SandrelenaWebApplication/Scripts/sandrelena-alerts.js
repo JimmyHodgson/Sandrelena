a8148af32156c8557ca9b862cd6d0bd33851b7a5
Jimmy Hodgson <Jimmy.hodgson@outlook.com>
2016-05-13 17:14:14 -0600

    Se agregaron los modales de alerta. NOT TESTED.

---
+++ SandrelenaWebApplication/SandrelenaWebApplication/Scripts/sandrelena-alerts.js
@@ -0,0 +1,33 @@
+﻿$(document).on('show.bs.modal', '.modal', function () {
+    var zIndex = 1040 + (10 * $('.modal:visible').length);
+    $(this).css('z-index', zIndex);
+    setTimeout(function () {
+        $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack');
+    }, 0);
+});
+
+$(document).on('hidden.bs.modal', '.modal', function () {
+    $('.modal:visible').length && $(document.body).addClass('modal-open');
+});
+
+function showAlert(element, title, message) {
+    $(element + ' .alert-title').html(title);
+    $(element + ' .alert-message').html(message);
+
+    $(element).modal('show');
+}
+
+function confirmModal(callback) {
+    showAlert('#cw-alert', 'Confirm', '¿Esta seguro que quiere realizar esta acción?');
+
+    $("#btn-confirm").off().on('click', function () {
+        $("#cw-alert").modal('hide');
+        callback(true);
+
+    });
+    $("#btn-deny").off().on('click', function () {
+        $("#cw-alert").modal('hide');
+        callback(false);
+
+    });
+}
