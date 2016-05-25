00c5eb16f991e7d46967c90576fcd6a865942410
Kevin <kevmusic@hotmail.es>
2016-05-24 22:38:50 -0600

    Consultas listas

--- SandrelenaWebApplication/SandrelenaWebApplication/Views/index_test.aspx.cs
+++ SandrelenaWebApplication/SandrelenaWebApplication/Views/index_test.aspx.cs
@@ -13,7 +13,9 @@ namespace SandrelenaWebApplication.Views
         protected void Page_Load(object sender, EventArgs e)
         {
             SandrelenaCS db = new SandrelenaCS();
+            /********************************************************************/
             /***** PROFESOR *****/
+            /********************************************************************/
             /*Todas las clases que está dando el profesor logueado (idUsuario) */
             int id_profesor = 3;
             var profesor = db.Usuarios.Find(id_profesor);
@@ -21,6 +23,8 @@ namespace SandrelenaWebApplication.Views
             var clases_profesor = db.Asignaturas.Where(o => o.Usuarios == profesor);
             /********************************************************************/
             /*Los alumnos de la clase que seleccione (idAsignatura) */
+            /*Nombres y horarios de las clases que puede inscribir*/
+
             int id_horario = 1;
             var matriculas = db.Matriculas.Where(o => o.id_horario == id_horario);
             List<Usuarios> estudiantes = new List<Usuarios>();
@@ -36,13 +40,73 @@ namespace SandrelenaWebApplication.Views
             var clase = db.Asignaturas.Find(horario.id_asignatura);
             var nota = matricula.nota;
 
-
+            /********************************************************************/
             /***** ALUMNOS *****/
+            /********************************************************************/
             /*Ver las clases que puede inscribir el estudiante logueado (idUsuario)*/
             int id_estudiante = 6;
             var estudiante = db.Usuarios.Find(id_estudiante);
             matriculas = db.Matriculas.Where(o => o.id_estudiante == id_estudiante);
 
+            // Carreras del estudiante
+            var carreras = db.Carreras.Where(o => o.Usuarios.Where(x => x.id_usuario == id_estudiante).First().id_usuario == id_estudiante);
+            List<Semestres> semestres = new List<Semestres>();
+            List<Asignaturas> clases = new List<Asignaturas>();
+            foreach (var ca in carreras)
+            {
+                // Semestres de las carreras
+                foreach (var sm in ca.Semestres)
+                {
+                    // Clases de los semestres
+                    foreach (var cl in sm.Asignaturas)
+                    {
+                        clases.Add(cl);
+                    }
+                }
+            }
+
+            var horarios_disponibles = db.Horarios.Where(o => o.isActive == true);
+            foreach (var ma in matriculas)
+            {
+                // Excluyendo las ya matriculadas
+                horarios_disponibles = horarios_disponibles.Where(o => o.id_horario != ma.id_horario);
+                foreach (var cl in clases)
+                {
+                    // Excluyendo las que no son de la carrera
+                    horarios_disponibles = horarios_disponibles.Where(o => o.id_asignatura == cl.id_asignatura);
+                }
+            }
+            /********************************************************************/
+            /***** ADMINISTRACIÓN *****/
+            /********************************************************************/
+            /*Crear una nueva clase*/
+            Asignaturas nuevaClase = new Asignaturas();
+            nuevaClase.codigo_asignatura = 0314;
+            nuevaClase.nombre_asignatura = "Ingeniería Ambiental I";
+            nuevaClase.creditos = 4;
+
+            db.Asignaturas.Add(nuevaClase);
+            db.SaveChanges();
+            /********************************************************************/
+            /*Darle horarios a la clase*/
+            // Filtrar los horarios que ya están asignados
+            id_horario = 7;
+            horario = db.Horarios.Find(7);
+            db.Horarios.Attach(horario);
+            nuevaClase.Horarios.Add(horario);
+
+            db.Entry(nuevaClase).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            /********************************************************************/
+            /*Ver todas las clases*/ // Carlos, plz...
+            var allClases = db.Asignaturas;
+            /********************************************************************/
+            /*Cargar los horarios de todas las clases*/
+            foreach (var cl in db.Asignaturas)
+            {
+                var horarios_asignatura = db.Horarios.Where(o => o.id_asignatura == cl.id_asignatura);
+            }
         }
     }
 }
+ 
